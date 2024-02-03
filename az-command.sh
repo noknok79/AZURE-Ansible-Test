905f678e2ea17dbb112bcdeb542cac79b119ba9c
root <root@LAPTOP-OJCSKH2M>
2024-02-03 16:35:38 +0800

    change from southeastasia to westus region

--- az-command.sh
+++ az-command.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+echo "" > inventory.txt
 az network public-ip list --query '[].ipAddress' --output tsv >> inventory.txt
 
 az vm list --query "[].{Name:name, PublicIP:publicIps[0], OS:storageProfile.osDisk.osType, Offer:storageProfile.imageReference.offer}" --output table >> inventory.txt
 
+cat inventory.txt
